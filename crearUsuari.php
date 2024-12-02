d38e670d78ec1b1eb9622dbe9fed7e9465113edb
Marc <maarcnavarro9@gmail.com>
2024-12-02 17:49:27 +0100

    Update crearUsuari.php

--- crearUsuari.php
+++ crearUsuari.php
@@ -12,22 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit;
     }
 
-    // Determinar la taula en funció de la checkbox
-    $table = $isPersonal ? 'personal' : 'client';
-
-    // Verificar si l'usuari ja existeix a la taula corresponent
-    $checkQuery = "SELECT * FROM $table WHERE nick = ?";
-    $stmt = $conn->prepare($checkQuery);
-    $stmt->bind_param("s", $newUser);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if ($result->num_rows > 0) {
-        echo "Error: L'usuari ja existeix a la taula $table. Tria un altre nom.";
-        exit;
-    }
-
-    // Si l'usuari no existeix, inserir el nou usuari
+    // Inserir el nou usuari
     $salt = '$2y$10$' . substr(str_replace('+', '.', base64_encode(random_bytes(16))), 0, 22);
     $hashedPass = crypt($newPass, $salt);
     $insertQuery = "INSERT INTO $table (nick, contrasenya) VALUES (?, ?)";
